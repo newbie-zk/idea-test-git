d29e044df1c5f7b82653c7d85c76f727f1084aa3
newbie-zk <newbie_zk@qq.com>
2020-06-20 20:02:27 +0800

    第一次用idea创建git

---
+++ gitTest/src/com/newbie/domain/GitTest.java
@@ -0,0 +1,10 @@
+package com.newbie.domain;
+
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("Hello Git !");
+        System.out.println("Hello Git project!");
+        System.out.println("Hello Git project!");
+        System.out.println("Hello Git project!");
+    }
+}
